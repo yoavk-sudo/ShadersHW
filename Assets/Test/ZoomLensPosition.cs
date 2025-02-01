0b7d1e911f88babac4fa5f1e029d0fd009a2a290
yoavk-sudo <kenkendler123@gmail.com>
2025-02-01 15:30:12 +0200

    test

---
+++ Assets/Test/ZoomLensPosition.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ZoomLensPosition : MonoBehaviour
+{
+    [SerializeField] Material _zoomMaterial;
+    Camera _camera;
+    Vector2 _screenPos;
+
+    private const string SHADER_OBJECT_SCREEN_POSITION = "_ObjectScreenPosition";
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        _screenPos = _camera.WorldToScreenPoint(transform.position);
+        _screenPos = new Vector2(_screenPos.x / Screen.width, _screenPos.y / Screen.height);
+        _zoomMaterial.SetVector(SHADER_OBJECT_SCREEN_POSITION, _screenPos);
+    }
+}
